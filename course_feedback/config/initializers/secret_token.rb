4a14797dc61004690a1d4d6045aa9d6c35e28e60
Thilina Gunarathne <csethil@gmail.com>
2012-11-03 14:11:15 -0400

    first

---
+++ course_feedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = '201f89f32b04857acb3a404464e4f3c88895713aeb3e957276e7da3fd76c51129bfda43559faebb57225aa06cf0a14c424872091efe7f26fe73410a7d3f47c2d'
